4327c24d2e49d75f5c8bddc2865bb82503eed7e1
Glitch (hello-webpage) <none>
2021-06-07 16:27:56 +0000

    📠🎌 Checkpoint ./index.html:3856783/12 ./script.js:3856783/316 ./sketch.js:3856783/938

--- sketch.js
+++ sketch.js
@@ -1,9 +1,43 @@
+
+/* VELOCITY
+let x;
+let xVelocity;
+
 function setup() {
-  createCanvas(600, 500);
+  createCanvas(400, 400);
+  x = 200;
+  xVelocity = 5;
 }
 
 function draw() {
-  background(230);
-  // Delete the slashes on the next line to "uncomment" the ellipse
-  // ellipse(50, 50, 50, 50)
+  ellipse(x, 50, 25, 25);
+  x = x + xVelocity; // Change the position based on current velocity
 }
+
+// END OF VELOCITY 
+*/
+
+let y;
+let yVelocity;
+let gravity;
+
+function setup() {
+  createCanvas(400, 400);
+  y = 0;
+  yVelocity = 0;
+  gravity = 0.5;
+}
+
+function draw() {
+  ellipse(200, 50, 25, 25);
+  y += yVelocity; // Change the position based on current velocity
+  yVelocity += gravity; // Change the velocity based on gravity
+  bounce(); 
+}
+
+function bounce() {
+  if (y >= height) { // if the ball hits the bottom of the screen...
+    yVelocity = -1 * yVelocity; // ...reverse its velocity.
+  }
+}
+
