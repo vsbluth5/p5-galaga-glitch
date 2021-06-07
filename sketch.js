c7347441a73482d108c140fffce952ea60f56a81
Glitch (hello-webpage) <none>
2021-06-07 16:55:44 +0000

    🌔🌪 Checkpoint ./sketch.js:3856783/1695 ./index.html:3856783/58

--- sketch.js
+++ sketch.js
@@ -1,38 +1,54 @@
-let birdY, fallSpeed, gravity, pipePos
+let birdY, fallSpeed, gravity, pipePos, score;
 
 function setup() {
-  createCanvas(800, 600)
-  birdY = 300
-  fallSpeed = -5
-  gravity = 0.3
-  
-  pipePos = width
+  createCanvas(800, 600);
+  birdY = 300;
+  fallSpeed = -5;
+  gravity = 0.1;
+
+  score = 0;
+
+  pipePos = width;
 }
 
 function draw() {
-  background(220)
-  ellipse(200, birdY, 50, 50)
-  birdY += fallSpeed
-  fallSpeed += gravity
-  
+  background(220);
+  // add scoring
+  text("score: " + score, 50, 200);
+
+  ellipse(200, birdY, 50, 50);
+  birdY += fallSpeed;
+  fallSpeed += gravity;
+
   // let the bird bounce off bottom
   if (birdY + 25 > height) {
-  fallSpeed = -fallSpeed * 0.8
-}
-  
+    fallSpeed = -fallSpeed * 0.8;
+  }
+
   // draw the moving pipe
-  rect(pipePos, 0, 50, 250)
-rect(pipePos, 350, 50, height - 350)
-  pipePos -= 1
-  
+  rect(pipePos, 0, 50, 250);
+  rect(pipePos, 350, 50, height - 350);
+  pipePos -= 1;
+
   if (pipePos < -50) {
-  pipePos = width
+    score += 1
+    pipePos = width;
+  }
+  
+  // function takes seven paramters: rectX, rectY, rectWidth, rectHeight, circleX, circleY, circleDiameter
+  if (collideRectCircle(pipePos, 0, 50, 250, 200, birdY, 50)) {
+  score = 0
 }
+  
+  if (collideRectCircle(pipePos, 350, 50, height - 350, 200, birdY, 50)) {
+  score = 0
+}
+
+
 
 
 }
 
 function keyPressed() {
-  fallSpeed = -10
+  fallSpeed = -10;
 }
-
